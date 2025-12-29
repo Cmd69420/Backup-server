47a516c546162148994a1d4e22e1b4fb2a12b6fe
36px4 <150375204+Cmd69420@users.noreply.github.com>
2025-12-29 18:10:46 +0000

    Dashboard

--- config/constants.js
+++ config/constants.js
@@ -8,7 +8,7 @@ export const MIDDLEWARE_TOKEN = process.env.MIDDLEWARE_TOKEN || "tally-middlewar
 export const CORS_ORIGIN = [
   "http://localhost:3000", 
   "https://geo-track-em3s.onrender.com",
-  "https://dashboard-1yda.onrender.com"
+  "https://dashboard-tsw3.onrender.com"
 ];
 
 // 🆕 EMAIL CONFIGURATION
